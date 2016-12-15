268ecd5ca578e8ab0b9ed564f9896319564e1f28
skylark345 <amirnurbekov756@gmail.com>
2016-12-15 17:08:45 +0600

    I have modified main.js file, which change colors in rgb spectre

--- main.js
+++ main.js
@@ -3,5 +3,35 @@ var gl = canvas.getContext("webgl");
 
 
 
-gl.clearColor(1.0, 0.0, 0.0, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
+
+var t0 = performance.now();
+var red = 0;
+var timeElapsed = 0;
+
+
+function frame() {
+  var t1 = performance.now();
+  var diff = t1 - t0;
+  t0 = performance.now();
+
+    gl.clearColor(1 - timeElapsed/5000, timeElapsed/5000, 0.0, 1.0);
+
+
+    if(timeElapsed > 5000 && timeElapsed < 10000){
+      gl.clearColor(0.0, 1 - (timeElapsed - 5000)/5000, (timeElapsed - 5000)/5000, 1.0);
+
+    }
+
+    else if(timeElapsed > 10000){
+      gl.clearColor((timeElapsed - 10000)/5000, 0.0, 1 - (timeElapsed - 10000)/5000, 1.0);
+      timeElapsed = 0;
+    }
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  timeElapsed+= diff;
+  requestAnimationFrame(frame);
+
+}
+
+
+  frame();
