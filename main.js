b99cb73895dab21fe48c19dd3e080435372c4f3e
skylark345 <amirnurbekov756@gmail.com>
2016-12-16 14:02:17 +0600

    Updated files: index.html, main.js

--- main.js
+++ main.js
@@ -1,37 +1,190 @@
 var canvas = document.getElementById("c");
 var gl = canvas.getContext("webgl");
 
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
 
 
-
-var t0 = performance.now();
-var red = 0;
-var timeElapsed = 0;
+initShaders();
 
 
-function frame() {
-  var t1 = performance.now();
-  var diff = t1 - t0;
-  t0 = performance.now();
+function initShaders() {
+  var fragmentShader = getShader(gl, "shader-fs");
+  var vertexShader = getShader(gl, "shader-vs");
 
-    gl.clearColor(1 - timeElapsed/5000, timeElapsed/5000, 0.0, 1.0);
+  // создать шейдерную программу
 
+  var shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
 
-    if(timeElapsed > 5000 && timeElapsed < 10000){
-      gl.clearColor(0.0, 1 - (timeElapsed - 5000)/5000, (timeElapsed - 5000)/5000, 1.0);
+  // Если создать шейдерную программу не удалось, вывести предупреждение
 
-    }
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert("Unable to initialize the shader program.");
+  }
 
-    else if(timeElapsed > 10000){
-      gl.clearColor((timeElapsed - 10000)/5000, 0.0, 1 - (timeElapsed - 10000)/5000, 1.0);
-      timeElapsed = 0;
-    }
+  gl.useProgram(shaderProgram);
 
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  timeElapsed+= diff;
-  requestAnimationFrame(frame);
-
+  vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+  gl.enableVertexAttribArray(vertexPositionAttribute);
 }
 
 
-  frame();
+
+
+
+
+
+
+function getShader(gl, id) {
+  var shaderScript, theSource, currentChild, shader;
+
+  shaderScript = document.getElementById(id);
+
+  if (!shaderScript) {
+    return null;
+  }
+
+  theSource = "";
+  currentChild = shaderScript.firstChild;
+
+  while(currentChild) {
+    if (currentChild.nodeType == currentChild.TEXT_NODE) {
+      theSource += currentChild.textContent;
+    }
+
+    currentChild = currentChild.nextSibling;
+  }
+
+
+
+
+  if (shaderScript.type == "x-shader/x-fragment") {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+  } else if (shaderScript.type == "x-shader/x-vertex") {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  } else {
+     // неизвестный тип шейдера
+     return null;
+  }
+
+
+
+
+  gl.shaderSource(shader, theSource);
+
+  // скомпилировать шейдерную программу
+  gl.compileShader(shader);
+
+  // Проверить успешное завершение компиляции
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+      return null;
+  }
+
+  return shader;
+}
+
+
+
+
+
+var horizAspect = 600.0/800.0;
+
+function initBuffers() {
+  squareVerticesBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+
+  var vertices = [
+    1.0,  1.0,  0.0,
+    -1.0, 1.0,  0.0,
+    1.0,  -1.0, 0.0,
+    -1.0, -1.0, 0.0
+  ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+}
+
+
+
+
+
+
+function drawScene() {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  perspectiveMatrix = makePerspective(45, 800.0/600.0, 0.1, 100.0);
+
+  loadIdentity();
+  mvTranslate([-0.0, 0.0, -6.0]);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+  gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+  setMatrixUniforms();
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
+
+
+
+
+
+function loadIdentity() {
+  mvMatrix = Matrix.I(4);
+}
+
+function multMatrix(m) {
+  mvMatrix = mvMatrix.x(m);
+}
+
+function mvTranslate(v) {
+  multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
+}
+
+function setMatrixUniforms() {
+  var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+  gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
+
+  var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+  gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
+}
+
+
+
+
+
+// var t0 = performance.now();
+// var red = 0;
+// var timeElapsed = 0;
+
+
+// function frame() {
+//   var t1 = performance.now();
+//   var diff = t1 - t0;
+//   t0 = performance.now();
+//
+//     gl.clearColor(1 - timeElapsed/5000, timeElapsed/5000, 0.0, 1.0);
+//
+//
+//     if(timeElapsed > 5000 && timeElapsed < 10000){
+//       gl.clearColor(0.0, 1 - (timeElapsed - 5000)/5000, (timeElapsed - 5000)/5000, 1.0);
+//
+//     }
+//
+//     else if(timeElapsed > 10000){
+//       gl.clearColor((timeElapsed - 10000)/5000, 0.0, 1 - (timeElapsed - 10000)/5000, 1.0);
+//       timeElapsed = 0;
+//     }
+//
+//   gl.clear(gl.COLOR_BUFFER_BIT);
+//   timeElapsed+= diff;
+//   requestAnimationFrame(frame);
+//
+//
+//
+//
+//
+//
+// }
