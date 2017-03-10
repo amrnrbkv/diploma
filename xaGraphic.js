0b8850de632f45dd5a14de6d24edacba1ce1de86
msta@gmail.com <zprk.tunguska@mail.ru>
2017-03-11 00:56:20 +0600

    new age

--- xaGraphic.js
+++ xaGraphic.js
@@ -1,21 +1,24 @@
-var xaGraphic={
+var xaGraphic = new function(){
 	var      xaCanvas;
 	var      xaGL;
 	var      xaConstObjects;
 	var      xaCamera;
+	var      xaDoublePI;
+	var      xaHalfPI;
+	var      xaWorldCenter;
 	var      xaNumOfDwnlds;
 	var      xaExternalCallBackFunc;
 	var      xaSimpleTexturedShader;
 	
 	function xaCallBackIfReady(){
-		if(xaNumOfDwnlds===0){
+		if(xaNumOfDwnlds==0){
 			xaExternalCallBackFunc();
 		}
 	}
 	
 	function xaDwnldXAM(addressIn,callbackIn){
 		var request = new XMLHttpRequest();
-		request.open("GET", "addressIn", true);
+		request.open("GET", addressIn, true);
 		request.responseType = "arraybuffer";
 		request.send();
 		xaNumOfDwnlds++;
@@ -24,8 +27,8 @@ var xaGraphic={
 			if(request.readyState === XMLHttpRequest.DONE){
 				var data               = new DataView(request.response);
 				var indexOfElements    = data.getUint32(0, true);
-				returnValue.modelElems = new Uint16Array(qwerty.response, indexOfElements);
-				returnValue.modelCoors = new Float32Array(qwerty.response, 4, (indexOfElements - Uint32Array.BYTES_PER_ELEMENT)/Float32Array.BYTES_PER_ELEMENT);
+				returnValue.modelElems = new Uint16Array(request.response, indexOfElements);
+				returnValue.modelCoors = new Float32Array(request.response, 4, (indexOfElements - Uint32Array.BYTES_PER_ELEMENT)/Float32Array.BYTES_PER_ELEMENT);
 				returnValue.numOfElems = (data.byteLength - indexOfElements)/Uint16Array.BYTES_PER_ELEMENT;
 				xaNumOfDwnlds--;
 				callbackIn();
@@ -46,24 +49,22 @@ var xaGraphic={
 	}
 	
 	function xaCompileShader(shaderNameIn, shaderTypeIn){
-		var returnValue           = gl.createShader(shaderTypeIn);
-		gl.shaderSource(returnValue,document.getElementById(shaderNameIn).text);
-		gl.compileShader(returnValue);
-		if (!gl.getShaderParameter(returnValue, gl.COMPILE_STATUS){
-			alert("An error occurred compiling the shader: " + gl.getShaderInfoLog(returnValue));  
+		var returnValue           = xaGL.createShader(shaderTypeIn);
+		xaGL.shaderSource(returnValue,document.getElementById(shaderNameIn).text);
+		xaGL.compileShader(returnValue);
+		if (!xaGL.getShaderParameter(returnValue, xaGL.COMPILE_STATUS)){
+			alert("An error occurred compiling the shader: " + xaGL.getShaderInfoLog(returnValue));  
 		}
 		return returnValue;
 	}
 	
-	function xaLinkShaderPrg(vShaderIn, fShaderIn){
-		var returnValue = gl.createProgram();
-		gl.attachShader(returnValue, vShaderIn);
-        gl.attachShader(returnValue, fShaderIn);
-		gl.linkProgram(returnValue);
-		if (!gl.getProgramParameter(returnValue, gl.LINK_STATUS)) {
-			alert("An error occurred while linking the shaderProgram program: " + gl.getProgramInfoLog(returnValue));
+	function xaPrepareShaderPrg(vShaderIn, fShaderIn, programIn){
+		xaGL.attachShader(programIn, vShaderIn);
+        xaGL.attachShader(programIn, fShaderIn);
+		xaGL.linkProgram(programIn);
+		if (!xaGL.getProgramParameter(programIn, xaGL.LINK_STATUS)) {
+			alert("An error occurred while linking the shaderProgram program: " + xaGL.getProgramInfoLog(programIn));
 		}
-		return returnValue;
 	}
 	function xaCreateConstObj(XAMAddressIn,TextureAddressIn){
 		var constObj = new Object();
@@ -71,7 +72,7 @@ var xaGraphic={
 		var XAMdata = xaDwnldXAM(XAMAddressIn,function(){
 			constObj.arrBuff   = xaGL.createBuffer();
 			constObj.elemsBuff = xaGL.createBuffer();
-			constObj.numOfElems= XAMdata.numOfElems();
+			constObj.numOfElems= XAMdata.numOfElems;
 			xaGL.bindBuffer(xaGL.ARRAY_BUFFER, constObj.arrBuff);
 			xaGL.bufferData(xaGL.ARRAY_BUFFER, XAMdata.modelCoors, xaGL.STATIC_DRAW);
 			xaGL.bindBuffer(xaGL.ELEMENT_ARRAY_BUFFER, constObj.elemsBuff);
@@ -79,40 +80,43 @@ var xaGraphic={
 			xaCallBackIfReady();
 		});
 		var img = xaDwnldTexture(TextureAddressIn,function(){
-			constObj.texIndx = gl.createTexture();
-           // gl.activeTexture(gl.TEXTURE0);
-            gl.bindTexture(gl.TEXTURE_2D, textureIndex);
-            gl.texImage2D (gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+			constObj.texIndx = xaGL.createTexture();
+            xaGL.bindTexture(xaGL.TEXTURE_2D, constObj.texIndx);
+            xaGL.texImage2D (xaGL.TEXTURE_2D, 0, xaGL.RGBA, xaGL.RGBA, xaGL.UNSIGNED_BYTE, img);
+            xaGL.texParameteri(xaGL.TEXTURE_2D, xaGL.TEXTURE_WRAP_S, xaGL.CLAMP_TO_EDGE);
+            xaGL.texParameteri(xaGL.TEXTURE_2D, xaGL.TEXTURE_WRAP_T, xaGL.CLAMP_TO_EDGE);
+            xaGL.texParameteri(xaGL.TEXTURE_2D, xaGL.TEXTURE_MIN_FILTER, xaGL.LINEAR);
+			xaCallBackIfReady();
 
-            //gl.uniform1i(uniformLoc_, 0);
 		});
 		xaConstObjects.push(constObj);
 	}
 
-	this.Initialize(canvasIn, callbackIn){
+	this.Initialize = function(canvasIn, callbackIn){
 		xaCanvas               = canvasIn;
 		xaCanvas.width         = window.innerWidth;
 		xaCanvas.height        = window.innerHeight;
-		xaGL                   = canvas.getContext("webgl");
+		xaGL                   = xaCanvas.getContext("webgl");
 		xaExternalCallBackFunc = callbackIn;
 		xaConstObjects         = [];
 		xaNumOfDwnlds          = 0;
 		//xaPushConstObj("models/cubemap8x8.xam");
 		xaSimpleTexturedShader = new Object();
 		xaCamera               = new Object();
-		xaSimpleTexturedShader.program = gl.createProgram();
-		xaLinkShaderPrg(xaCompileShader("vShader",gl.VERTEX_SHADER), xaCompileShader("fShader",gl.FRAGMENT_SHADER));
-		xaSimpleTexturedShader.matLoc  = xaSimpleTexturedShader.program(shaderProgram, "matrix_");
-		xaSimpleTexturedShader.texLoc  = xaSimpleTexturedShader.program(shaderProgram, "texture_");
-		
+		xaSimpleTexturedShader.program = xaGL.createProgram();
+		xaPrepareShaderPrg(xaCompileShader("vShader",xaGL.VERTEX_SHADER),
+						   xaCompileShader("fShader",xaGL.FRAGMENT_SHADER),
+						   xaSimpleTexturedShader.program);
+		xaSimpleTexturedShader.matLoc  = xaGL.getUniformLocation(xaSimpleTexturedShader.program, "matrix_");
+		xaSimpleTexturedShader.texLoc  = xaGL.getUniformLocation(xaSimpleTexturedShader.program, "texture_");
+		xaWorldCenter       = vec3.fromValues(0,0,0);
 		xaCamera.position   = vec3.fromValues(0,0,0);
 		xaCamera.rotationY  = 0;
 		xaCamera.rotationX  = 0;
 		xaCamera.perspMat4  = mat4.create();
 		mat4.perspective(xaCamera.perspMat4, 1.57, xaCanvas.width/xaCanvas.height, 0.25, 50.0);
+		xaDoublePI = 2*Math.PI;
+		xaHalfPI   = Math.PI/2;
 		
 		xaCreateConstObj("models/cubemap8x8.xam","textures/cubemap_texture.png");
 		
@@ -121,32 +125,57 @@ var xaGraphic={
 		xaGL.clearColor(0.5, 0.5, 0.5, 1.0);
 		xaGL.enable(xaGL.DEPTH_TEST);
 		xaGL.clearDepth(1);
+		
+		window.onresize = function() {
+			xaCanvas.width  = xaCanvas.clientWidth;
+			xaCanvas.height = xaCanvas.clientHeight;
+			mat4.perspective(xaCamera.perspMat4, 1.57, xaCanvas.width/xaCanvas.height, 0.25, 50.0);
+			xaGL.viewport(0.0, 0.0, xaCanvas.width, xaCanvas.height);
+		};
 	}
-	this.RenderFrame(){
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	this.RenderFrame = function(){
+		xaGL.clear(xaGL.COLOR_BUFFER_BIT | xaGL.DEPTH_BUFFER_BIT);
 		var resultMatrix = mat4.create();
-		mat4.rotateX(resultMatrix4x4, xaCamera.perspMat4, xaCamera.rotationX);
-		mat4.rotateY(resultMatrix4x4, resultMatrix4x4, xaCamera.rotationY);
-		mat4.translate(resultMatrix4x4, resultMatrix4x4, vec3.fromValues(-xaCamera.position[0], -xaCamera.position[1], -xaCamera.position[2]));
+		mat4.rotateX(resultMatrix, xaCamera.perspMat4, xaCamera.rotationX);
+		mat4.rotateY(resultMatrix, resultMatrix, xaCamera.rotationY);
+		mat4.translate(resultMatrix, resultMatrix, vec3.fromValues(-xaCamera.position[0], -xaCamera.position[1], -xaCamera.position[2]));
 
 		
 		
 		//
-		gl.enableVertexAttribArray(0);
-		gl.enableVertexAttribArray(1);
-		gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, Float32Array.BYTES_PER_ELEMENT * 5, 0);
-		gl.vertexAttribPointer(1, 2, gl.FLOAT, gl.FALSE, Float32Array.BYTES_PER_ELEMENT * 5, Float32Array.BYTES_PER_ELEMENT * 3);
+		xaGL.enableVertexAttribArray(0);
+		xaGL.enableVertexAttribArray(1);
+		xaGL.vertexAttribPointer(0, 3, xaGL.FLOAT, xaGL.FALSE, Float32Array.BYTES_PER_ELEMENT * 5, 0);
+		xaGL.vertexAttribPointer(1, 2, xaGL.FLOAT, xaGL.FALSE, Float32Array.BYTES_PER_ELEMENT * 5, Float32Array.BYTES_PER_ELEMENT * 3);
 		//
-		gl.activeTexture(gl.TEXTURE0);
+		xaGL.activeTexture(xaGL.TEXTURE0);
 		//
-		gl.useProgram(xaSimpleTexturedShader.program);
+		xaGL.useProgram(xaSimpleTexturedShader.program);
 		for (var i = 0; i < xaConstObjects.length; i++) {
-			gl.bindTexture(gl.TEXTURE_2D, xaConstObjects[i].texIndx);
-			gl.uniform1i(xaSimpleTexturedShader.texLoc, 0);
-			gl.uniformMatrix4fv(xaSimpleTexturedShader.matLoc, gl.FALSE, resultMatrix4x4);
+			xaGL.bindTexture(xaGL.TEXTURE_2D, xaConstObjects[i].texIndx);
+			xaGL.uniform1i(xaSimpleTexturedShader.texLoc, 0);
+			xaGL.uniformMatrix4fv(xaSimpleTexturedShader.matLoc, xaGL.FALSE, resultMatrix);
 			xaGL.bindBuffer(xaGL.ARRAY_BUFFER, xaConstObjects[i].arrBuff);
 			xaGL.bindBuffer(xaGL.ELEMENT_ARRAY_BUFFER, xaConstObjects[i].elemsBuff);
-			gl.drawElements(gl.TRIANGLES, xaConstObjects[i].numOfElems, gl.UNSIGNED_SHORT, 0);
+			xaGL.drawElements(xaGL.TRIANGLES, xaConstObjects[i].numOfElems, xaGL.UNSIGNED_SHORT, 0);
 		}	
+	}
+	this.MoveCamera   = function(deltaPosIn){
+		var tempVec = vec3.create();
+		vec3.rotateX(tempVec, deltaPosIn, xaWorldCenter, -xaCamera.rotationX);
+		vec3.rotateY(tempVec, tempVec, xaWorldCenter, -xaCamera.rotationY);
+		vec3.add(xaCamera.position, xaCamera.position, tempVec);
+	}
+	this.RotateCamera = function(deltaXRotIn,deltaYRotIn){
+		xaCamera.rotationY += deltaYRotIn;
+		xaCamera.rotationX += deltaXRotIn;
+		if(xaCamera.rotationY>xaDoublePI || xaCamera.rotationY<-xaDoublePI){
+			xaCamera.rotationY=0;
+		}
+		if (xaCamera.rotationX > xaHalfPI) {
+			xaCamera.rotationX = xaHalfPI;
+        } else if (xaCamera.rotationX < -xaHalfPI) {
+            xaCamera.rotationX = -xaHalfPI;
+        }
 	}
 }
