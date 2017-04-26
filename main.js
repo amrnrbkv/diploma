8e20d41d9723a11d147540e7ebd49789a586c17a
skylark345 <amirnurbekov756@gmail.com>
2017-04-27 02:30:04 +0600

    New update

--- main.js
+++ main.js
@@ -1,144 +1,180 @@
 
-var IPAddress = document.getElementById('ip');
-var nickname = document.getElementById('nickname');
-var playButton = document.getElementsByTagName("button");
-var data;
-var visible = document.getElementsByClassName('visible');
-
-function ValidateIPaddress(){
- if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(IPAddress.value))
-  {
-    return (true);
-  }
-
-  IPAddress.style.borderColor = "red";
-  IPAddress.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
-  visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
-  alert("You have entered an invalid IP address!");
-  return (false);
-}
-
-
-function ValidateNickname(){
-  if(nickname.value.length == 0){
-    nickname.style.borderColor = "red";
-    nickname.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
-    visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
-    alert("Your nickname should contain at least one symbol");
-    return false;
-  }
-
-  else if(nickname.value.length > 10){
-    nickname.style.borderColor = "red";
-    nickname.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
-    visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
-    alert("Your nickname should contain no more than 10 symbols");
-    return false;
-  }
-
-  else{
-    return true;
-  }
-}
-
-
-
-
-function initConnection(){
-  var address = 'ws://' + IPAddress.value + ':8888';
-  var socket = new WebSocket(address);
-  var ready = false;
-  socket.binaryType = "arraybuffer";
-
-  socket.addEventListener('open', function (event) {
-
-    console.log("Connection was successfully installed!!!");
-    var encodedText;
-    var dataSize = nickname.value.length + 2;
-    var data = new ArrayBuffer(dataSize);
-    var view = new DataView(data);
-    var encoder = new TextEncoder();
-    view.setUint16(0, 1001, true);
-    encodedText = encoder.encode(nickname.value);
-
-    for (var i = 0; i < nickname.value.length; i++) {
-      view.setUint8(i+2, encodedText[i], true);
-    }
-
-    socket.send(data);
-
-  });
-
-  socket.addEventListener('message', function (event) {
-    console.log(event.data);
-    var dataview = new DataView(event.data);
-    var code = dataview.getUint16(0, true);
-    console.log(code);
-
-    if(code == 2001){
-      document.getElementById('win').style.display='none';
-      start();
-    }
-
-
-    else if(code == 2002){
-      nickname.value = "Your nick is already used";
-      nickname.style.borderColor = "red";
-      nickname.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
-      IPAddress.style.color = "red";
-      visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
-    }
-
-    else if(code == 2003){
-      IPAddress.value = "This server is full!!!";
-      IPAddress.style.borderColor = "red";
-      IPAddress.style.color = "red";
-      IPAddress.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
-      visible[0].style.color = "linear-gradient(135deg, white, #ff0000);";
-    }
-
-    else {
-      IPAddress.value = "Unknown Error!!!";
-      IPAddress.style.borderColor = "red";
-      IPAddress.style.color = "red";
-      IPAddress.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
-      nickname.value = "Unknown Error!!!";
-      nickname.style.borderColor = "red";
-      nickname.style.color = "red";
-      nickname.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
-      visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
-    }
-
-  });
-
-
-
-    socket.onclose = function(){
-      document.getElementById('win').style.display ='inline';
-      
-      IPAddress.value = "Connection was terminated...";
-      IPAddress.style.color = "red";
-      IPAddress.style.borderColor = "red";
-      IPAddress.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
-      visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
-    };
-
-
-  return ready;
-
-};
-
-
-
-playButton[0].onclick = function(){
-  if (ValidateIPaddress() && ValidateNickname()) {
-      initConnection();
-  }
-};
-
-
-
-
+// var IPAddress = document.getElementById('ip');
+// var nickname = document.getElementById('nickname');
+// var playButton = document.getElementsByTagName("button");
+// var data;
+// var visible = document.getElementsByClassName('visible');
+//
+// function ValidateIPaddress(){
+//
+//  if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(IPAddress.value))
+//   {
+//     return (true);
+//   }
+//
+//   IPAddress.style.borderColor = "red";
+//   IPAddress.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
+//   visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
+//   alert("You have entered an invalid IP address!");
+//   return (false);
+// }
+//
+//
+// //////////////////////////////////////
+//
+//
+//
+// //////////////////////////////////////
+//
+//
+//
+//
+//
+//
+//
+//
+// function ValidateNickname(){
+//
+//
+//
+//   if(nickname.value.length == 0){
+//     nickname.style.borderColor = "red";
+//     nickname.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
+//     visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
+//     alert("Your nickname should contain at least one symbol");
+//     return false;
+//   }
+//
+//   else if(nickname.value.length > 10){
+//     nickname.style.borderColor = "red";
+//     nickname.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
+//     visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
+//     alert("Your nickname should contain no more than 10 symbols");
+//     return false;
+//   }
+//
+//   else{
+//     return true;
+//   }
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// function initConnection(){
+//   var address = 'ws://' + IPAddress.value + ':8888';
+//   var socket = new WebSocket(address);
+//   var ready = false;
+//   socket.binaryType = "arraybuffer";
+//
+//   socket.addEventListener('open', function (event) {
+//
+//     console.log("Connection was successfully installed!!!");
+//     var encodedText;
+//     var dataSize = nickname.value.length + 2;
+//     var data = new ArrayBuffer(dataSize);
+//     var view = new DataView(data);
+//     var encoder = new TextEncoder();
+//     view.setUint16(0, 1001, true);
+//     encodedText = encoder.encode(nickname.value);
+//
+//     for (var i = 0; i < nickname.value.length; i++) {
+//       view.setUint8(i+2, encodedText[i], true);
+//     }
+//
+//     socket.send(data);
+//
+//   });
+//
+//   socket.addEventListener('message', function (event) {
+//     console.log(event.data);
+//     var dataview = new DataView(event.data);
+//     var code = dataview.getUint16(0, true);
+//     console.log(code);
+//
+//     if(code == 2001){
+//       document.getElementById('win').style.display='none';
+//       start();
+//     }
+//
+//
+//     else if(code == 2002){
+//       nickname.value = "Your nick is already used";
+//       nickname.style.borderColor = "red";
+//       nickname.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
+//       IPAddress.style.color = "red";
+//       visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
+//     }
+//
+//     else if(code == 2003){
+//       IPAddress.value = "This server is full!!!";
+//       IPAddress.style.borderColor = "red";
+//       IPAddress.style.color = "red";
+//       IPAddress.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
+//       visible[0].style.color = "linear-gradient(135deg, white, #ff0000);";
+//     }
+//
+//     else {
+//       IPAddress.value = "Unknown Error!!!";
+//       IPAddress.style.borderColor = "red";
+//       IPAddress.style.color = "red";
+//       IPAddress.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
+//       nickname.value = "Unknown Error!!!";
+//       nickname.style.borderColor = "red";
+//       nickname.style.color = "red";
+//       nickname.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
+//       visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
+//     }
+//
+//   });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//     socket.onclose = function(){
+//       document.getElementById('win').style.display ='inline';
+//
+//       IPAddress.value = "Connection was terminated...";
+//       IPAddress.style.color = "red";
+//       IPAddress.style.borderColor = "red";
+//       IPAddress.style.boxShadow = "0px 0px 25px 5px rgba(255,0,0,0.5)";
+//       visible[0].style.background = "linear-gradient(135deg, white, #ff0000)";
+//     };
+//
+//
+//   return ready;
+//
+// };
+//
+//
+//
+// playButton[0].onclick = function(){
+//   if (ValidateIPaddress() && ValidateNickname()) {
+//       initConnection();
+//   }
+// };
+//
 
 
 
@@ -330,3 +366,9 @@ var start = function() {
 
 
 }
+
+
+
+
+
+start();
